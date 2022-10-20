c7f716fe4540d7da31660f333fa8430fa793e1f5
Miji <mjkim2304@gmail.com>
2022-10-20 13:52:39 +0900

    Tests for deleteA(), count()

--- src/test/java/UserDaoTest.java
+++ src/test/java/UserDaoTest.java
@@ -20,12 +20,42 @@ class UserDaoTest {
 
     @Test
     void addAndSelect() throws SQLException, ClassNotFoundException {
-        UserDao userDao = new UserDaoFactory().awsUserDao();
-        User user = new User("3", "EternityHwan","1123");
-        userDao.add(user);
+        User user1 = new User("1","박성철","1234");
 
-        User selectedUser = userDao.findById("3");
-        Assertions.assertEquals("EternityHwan", selectedUser.getName());
-        Assertions.assertEquals("1123", selectedUser.getPassword());
+        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
+
+        //컬럼삭제
+        userDao.deleteAll();
+        assertEquals(0,userDao.getCount());
+
+        userDao.add(user1);
+        assertEquals(1,userDao.getCount());
+        User user = userDao.findById(user1.getId());
+
+        Assertions.assertEquals(user1.getName(),user.getName());
+        Assertions.assertEquals(user1.getPassword(),user.getPassword());
+
+    }
+
+    @Test
+    void count() throws SQLException, ClassNotFoundException {
+        UserDao userDao = new UserDaoFactory().awsUserDao();
+
+        User user1 = new User("1","박성철","1234");
+        User user2 = new User("2","이길원","1321");
+        User user3 = new User("3","박범진","4321");
+
+        userDao.deleteAll();
+        assertEquals(0,userDao.getCount());
+
+        userDao.add(user1);
+        assertEquals(1,userDao.getCount());
+
+        userDao.add(user2);
+        assertEquals(2,userDao.getCount());
+
+        userDao.add(user3);
+        assertEquals(3,userDao.getCount());
+
     }
 }
