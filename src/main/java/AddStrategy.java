de133af7b2f60331c0267a8a3c65920118a4582e
Miji <mjkim2304@gmail.com>
2022-10-24 11:24:37 +0900

    AddStrategy

--- src/main/java/AddStrategy.java
+++ src/main/java/AddStrategy.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class AddStrategy implements StatementStrategy{
     @Override
     public PreparedStatement makePreparedStatement(Connection connection) throws SQLException {
-        PreparedStatement pstmt = connection.prepareStatement("insert into users (id, name, password) value(?,?,?)");
-        return null;
+        PreparedStatement ps = connection.prepareStatement("insert into users (id, name, password) value(?,?,?)");
+        return ps;
     }
 }
--- src/main/java/DeleteAllStrategy.java
+++ src/main/java/DeleteAllStrategy.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DeleteAllStrategy implements StatementStrategy{
     @Override
     public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-        return c.prepareStatement("delete from users");
-
+        PreparedStatement ps = c.prepareStatement("delete from users");
+        return ps;
     }
 }
